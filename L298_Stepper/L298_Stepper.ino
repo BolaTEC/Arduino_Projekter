30c2ec1037bd8a2cc691cf5e5cc1ab3b6209c89c
bolahcoe <76160871+BolaTEC@users.noreply.github.com>
2022-03-31 10:48:15 +0200

    Update L298_Stepper.ino
    Smal changes

--- L298_Stepper/L298_Stepper.ino
+++ L298_Stepper/L298_Stepper.ino
@@ -8,8 +8,9 @@ int delaytime = 2 ;
 int stepfw[4][4] = { {1,0,0,1} , {0,1,0,1} ,{0,1,1,0}, {1,0,1,0} };    //Step clockwise
 int stepbw[4][4] = { {1,0,1,0} , {0,1,1,0}, {0,1,0,1}, {1,0,0,1}  };   //Step anti clockwise 
 
-
-
+/***************************************/
+// SETUP
+/***************************************/
 void setup()                    
 {
   pinMode(in1, OUTPUT);    
@@ -22,11 +23,36 @@ void setup()
   digitalWrite(in3, LOW);   
   digitalWrite(in4, LOW);     
   Serial.begin(9600); 
+  Serial.println("Ready\n\n");
+  Serial.println("Give stepper order e.g. ");
+  Serial.println("(-100 for CCW 100 steps or 100 for CW 100 steps 0= stop)");
+}
+/***************************************/
+// LOOP
+/***************************************/
+void loop()                     
+{
+ int tal = readSerial();
+ if (tal>0)
+ {
+   Serial.println(tal);
+
+   for(int n=0; n <tal; n++) 
+     doStep(stepfw); 
+ }  
+ if ((tal<0)&&(tal!=-9999))
+ {
+   Serial.println(tal);
+
+   for(int n=0; n <tal*-1; n++) 
+     doStep(stepfw); 
+ }  
 }
 
-
+/***************************************/
+// DOSTEP
+/***************************************/
 void doStep(int steps[4][4]){
-
  for (int n=0 ; n < 4; n++){
    digitalWrite(in1, steps[n][0]);  
    digitalWrite(in2, steps[n][1]);  
@@ -34,20 +60,15 @@ void doStep(int steps[4][4]){
    digitalWrite(in4, steps[n][3]);  
    delay(delaytime);  
  }
-  
-  
 }
-
-
-void loop()                     
-{
- 
- for(int n=0; n <50; n++) 
-   doStep(stepfw); 
-
- for(int n=0; n <50; n++) 
-   doStep(stepbw); 
-  
-  
-  
+//***********************************
+// Read data from UART connection
+//***********************************
+int readSerial() {
+  int incomingByte = -9999;
+  while (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.parseInt();
+  }
+  return incomingByte;
 }
