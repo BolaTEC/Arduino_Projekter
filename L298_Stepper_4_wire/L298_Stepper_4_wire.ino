367f9085974990685a505aaa36b7e76b5964fc80
BolaTEC <bola@tec.dk>
2026-08-26 09:42:15 +0200

    Update L298_Stepper_4_wire.ino
    Stop funktion changed

--- L298_Stepper_4_wire/L298_Stepper_4_wire.ino
+++ L298_Stepper_4_wire/L298_Stepper_4_wire.ino
@@ -15,8 +15,8 @@ int in2 = 3;
 int in3 = 4;    
 int in4 = 5;    
 
-int delaytime = 2000; // Delay in Microseconds
-
+int delaytime = 900; // Delay in Microseconds
+int tal,oldTal=0;
 int stepfw[4][4] = { {1,0,1,0} , {0,1,1,0} ,{0,1,0,1}, {1,0,0,1} };    //Step clockwise
 int stepbw[4][4] = { {1,0,0,1} , {0,1,0,1}, {0,1,1,0}, {1,0,1,0}  };   //Step anti clockwise 
 
@@ -30,55 +30,52 @@ void setup()
   pinMode(in3, OUTPUT);        
   pinMode(in4, OUTPUT);     
  
-  digitalWrite(in1, HIGH); // Must be HIGH to avoid current consumption in idle state
-  digitalWrite(in2, HIGH);     
-  digitalWrite(in3, HIGH);   
-  digitalWrite(in4, HIGH);     
+  stopAll();
   Serial.begin(9600); 
   Serial.println("Ready\n\n");
   Serial.println("Give stepper order e.g. ");
   Serial.println("(-100 for CCW 100 steps or 100 for CW 100 steps 0= stop)");
   Serial.println("\n(>10000 for change delaytime (speed) e.g. 10500 gives 1500 ms delay)");
   Serial.println("(10000 < delay < 32765)");
-
-  
 }
 /***************************************/
 // LOOP
 /***************************************/
 void loop()                     
 {
- int tal = readSerial();
- if (tal==0)
- {
-   Serial.println(tal);
-  digitalWrite(in1, HIGH);   
-  digitalWrite(in2, HIGH);     
-  digitalWrite(in3, HIGH);   
-  digitalWrite(in4, HIGH);  
- }
- else if (tal>0)
- {
-    Serial.println(tal);
-    if (tal<10000)
+  tal = readSerial();
+  if (tal != oldTal)
+  {
+    if (tal==0)
     {
-      for(int n=0; n <tal; n++) 
-        doStep(stepfw);
+      Serial.println(tal);
+      stopAll();  
     }
-    else
+    else if (tal>0)
     {
-      delaytime = tal-9000;
-      Serial.print("delay= ");
-      Serial.println(delaytime,DEC);
-    } 
- }  
- else if ((tal<0)&&(tal!=-9999))
- {
-   Serial.println(tal);
+      Serial.println(tal);
+      if (tal<9999)
+      {
+        for(int n=0; n <tal; n++) 
+          doStep(stepfw);
+      }
+      else
+      {
+        delaytime = tal-10000;
+        Serial.print("delay= ");
+        Serial.println(delaytime,DEC);
+      } 
+    }  
+    else if ((tal<0)&&(tal!=-9999))
+    {
+      Serial.println(tal);
 
-   for(int n=0; n <tal*-1; n++) 
-     doStep(stepbw); 
- }  
+      for(int n=0; n <tal*-1; n++) 
+        doStep(stepbw); 
+    }
+    oldTal=tal;
+    stopAll();
+  }  
 }
 
 /***************************************/
@@ -93,12 +90,24 @@ void doStep(int steps[4][4]){
    delayMicroseconds(delaytime);  
  }
 }
+
+//***********************************
+// STOP ALL
+//***********************************
+void stopAll()
+{
+  digitalWrite(in1, HIGH); // Must be HIGH to avoid current consumption in idle state
+  digitalWrite(in2, HIGH);     
+  digitalWrite(in3, HIGH);   
+  digitalWrite(in4, HIGH);     
+}
+
 //***********************************
 // Read data from UART connection
 //***********************************
 int readSerial() {
   int incomingByte = -9999;
-  if (Serial.available() > 0) {
+  if (Serial.available() > 1) {
     // read the incoming byte:
     incomingByte = Serial.parseInt();
   }
